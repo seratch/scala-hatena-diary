9cc50da8ab3426aa579cf61f0c10b54e664a9a3e
Kazuhiro SERA <seratch@gmail.com>
2011-02-23 18:33:33 +0900

    Added S-99:P06-P10

--- scala-hatena-diary/src/main/scala/scalahatenadiary/scala99/problem01-10.scala
+++ scala-hatena-diary/src/main/scala/scalahatenadiary/scala99/problem01-10.scala
@@ -1,3 +1,6 @@
+/**
+ * http://aperiodic.net/phil/scala/s-99/
+ */
 package scalahatenadiary.scala99
 
 /**
@@ -94,6 +97,106 @@ object P05_ReverseAList {
       }
     }
     _reverse(Nil, list)
+  }
+
+}
+
+/**
+ * Find out whether a list is a palindrome.
+ * isPalindrome(List(1, 2, 3, 2, 1)) // => true
+ */
+object P06_FindOutWhehterAListIsAPalindrome {
+
+  def isPalindrome[T](list: List[T]): Boolean = list == list.reverse
+
+}
+
+/**
+ * Flatten a nested list structure.
+ * flatten(List(List(1, 1), 2, List(3, List(5, 8)))) // => List(1, 1, 2, 3, 5, 8)
+ */
+object P07_FlattenANestedListStructure {
+  //def flatten(list: List[Any]): List[Any] = list.flatMap {
+  //  case l: List[Any] => flatten(l)
+  //  case e => List(e)
+  //}
+  def flatten(list: List[_]): List[Any] = list.flatMap {
+    case l: List[_] => flatten(l)
+    case e => List(e)
+  }
+
+}
+
+/**
+ * Eliminate consecutive duplicates of list elements.
+ * If a list contains repeated elements they should be replaced with a single copy of the element.
+ * The order of the elements should not be changed.
+ * compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) // =>  List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e)
+ */
+object P08_EliminateConsecutiveDuplicatesOfListElements {
+
+  def compress[T](list: List[T]): List[T] = {
+    //def _compress(accumulator: List[T], list: List[T]): List[T] = {
+    //  list match {
+    //    case prev :: current :: tail => {
+    //      if (accumulator == Nil || prev != current) {
+    //        _compress(accumulator ::: List(current), current :: tail)
+    //      } else {
+    //        _compress(accumulator, current :: tail)
+    //      }
+    //    }
+    //    case _ => accumulator
+    //  }
+    //}
+    //_compress(Nil, list)
+    def _compress(accumulator: List[T], list: List[T]): List[T] = list match {
+      case head :: tail => _compress(head :: accumulator, tail.dropWhile(_ == head))
+      case Nil => accumulator.reverse
+    }
+    _compress(Nil, list)
+  }
+
+}
+
+/**
+ * Pack consecutive duplicates of list elements into sublists.
+ * If a list contains repeated elements they should be placed in separate sublists.
+ * pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+ * // => List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+ */
+object P09_PackConsecutiveDuplicatesOfListElementsIntoSubLists {
+
+  def pack[T](list: List[T]): List[List[T]] = {
+    def _pack(accumulator: List[List[T]], list: List[T]): List[List[T]] = list match {
+      case head :: tail => _pack(list.takeWhile(_ == head) :: accumulator, tail.dropWhile(_ == head))
+      case Nil => accumulator.reverse
+    }
+    _pack(Nil, list)
+  }
+}
+
+/**
+ * Run-length encoding of a list.
+ * Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+ * Consecutive duplicates of elements are encoded as tuples (N, E) where N is the number of duplicates of the element E.
+ * encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+ * // => List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+ */
+object P10_RunLengthEncodingOfAList {
+
+  import P09_PackConsecutiveDuplicatesOfListElementsIntoSubLists._
+
+  def encode[T](list: List[T]): List[Pair[Int, T]] = {
+    // pack(list) map (elem => (elem.length, elem.apply(0)))
+    pack(list) map (elem => (elem.length, elem.head))
+  }
+
+  def encode2[T](list: List[T]): List[Pair[Int, T]] = {
+    def _encode(accumulator: List[Pair[Int, T]], list: List[T]): List[Pair[Int, T]] = list match {
+      case head :: tail => _encode((list.takeWhile(_ == head).length, head) :: accumulator, tail.dropWhile(_ == head))
+      case Nil => accumulator.reverse
+    }
+    _encode(Nil, list)
   }
 
 }
--- scala-hatena-diary/src/test/scala/scalahatenadiary/scala99/problem01-10-specs.scala
+++ scala-hatena-diary/src/test/scala/scalahatenadiary/scala99/problem01-10-specs.scala
@@ -2,59 +2,120 @@ import org.specs.Specification
 import scalahatenadiary.scala99._
 
 object P01Spec extends Specification {
-  "P01_FindTheLastElementOfAList.last(List(1, 1, 2, 3, 5, 8)) " should {
-    "return 8 using built-in method" in {
+  "P01.last(List[T]) " should {
+    "run expectedly when using built-in method" in {
       P01_FindTheLastElementOfAList.last1(List(1, 1, 2, 3, 5, 8)) mustEqual 8
     }
-    "return 8" in {
+    "run expectedly" in {
       P01_FindTheLastElementOfAList.last2(List(1, 1, 2, 3, 5, 8)) mustEqual 8
     }
   }
 }
 
 object P02Spec extends Specification {
-  "P02_FindTheLastButOneElementOfAList.penultimate(List(1, 1, 2, 3, 5, 8)) " should {
-    "return 5" in {
+  "P02.penultimate(List[T]) " should {
+    "run expectedly" in {
       P02_FindTheLastButOneElementOfAList.penultimate(List(1, 1, 2, 3, 5, 8)) mustEqual 5
     }
   }
 }
 
 object P03Spec extends Specification {
-  "P03_FindTheNthElementOfAList.getNthElement(2,List(1, 1, 2, 3, 5, 8)) " should {
-    "return 2" in {
+  "P03.getNthElement(Int,List[T]) " should {
+    "run expectedly" in {
       P03_FindTheNthElementOfAList.getNthElement(2, List(1, 1, 2, 3, 5, 8)) mustEqual 2
     }
   }
-  "P03_FindTheNthElementOfAList.getNthElement2(2,List(1, 1, 2, 3, 5, 8)) " should {
-    "return 2" in {
+  "P03.getNthElement2(Int,List[T]) " should {
+    "run expectedly" in {
       P03_FindTheNthElementOfAList.getNthElement2(2, List(1, 1, 2, 3, 5, 8)) mustEqual 2
     }
   }
 }
 
 object P04Spec extends Specification {
-  "P04_FindTheNumberOfElementsOfAList.length(List(1, 1, 2, 3, 5, 8)) " should {
-    "return 6" in {
+  "P04.length(List[T]) " should {
+    "run expectedly" in {
       P04_FindTheNumberOfElementsOfAList.length(List(1, 1, 2, 3, 5, 8)) mustEqual 6
     }
   }
-  "P04_FindTheNumberOfElementsOfAList.length2(List(1, 1, 2, 3, 5, 8)) " should {
-    "return 6" in {
+  "P04.length2(List[T]) " should {
+    "run expectedly" in {
       P04_FindTheNumberOfElementsOfAList.length2(List(1, 1, 2, 3, 5, 8)) mustEqual 6
     }
   }
 }
 
 object P05Spec extends Specification {
-  "P05_ReverseAList.reverse(List(1, 1, 2, 3, 5, 8)) " should {
-    "return List(8, 5, 3, 2, 1, 1)" in {
+  "P05.reverse(List[T]) " should {
+    "run expectedly" in {
       P05_ReverseAList.reverse(List(1, 1, 2, 3, 5, 8)) mustEqual List(8, 5, 3, 2, 1, 1)
     }
   }
-  "P05_ReverseAList.reverse2(List(1, 1, 2, 3, 5, 8)) " should {
-    "return List(8, 5, 3, 2, 1, 1)" in {
+  "P05.reverse2(List[T]) " should {
+    "run expectedly" in {
       P05_ReverseAList.reverse2(List(1, 1, 2, 3, 5, 8)) mustEqual List(8, 5, 3, 2, 1, 1)
+    }
+  }
+}
+
+object P06Spec extends Specification {
+  "P06.isPalindrome(List[T]) " should {
+    "run expectedly" in {
+      P06_FindOutWhehterAListIsAPalindrome.isPalindrome(List(1, 2, 3, 2, 1)) mustEqual true
+    }
+  }
+  "P06.isPalindrome(List[T]) " should {
+    "run expectedly" in {
+      P06_FindOutWhehterAListIsAPalindrome.isPalindrome(List(1, 2, 3, 2, 2)) mustEqual false
+    }
+  }
+}
+
+object P07Spec extends Specification {
+  "P07.flatten(List[T]) " should {
+    "run expectedly" in {
+      P07_FlattenANestedListStructure.flatten(List(List(1, 1), 2, List(3, List(5, 8)))) mustEqual List(1, 1, 2, 3, 5, 8)
+    }
+  }
+}
+
+object P08Spec extends Specification {
+  "P08.compress(List[T])" should {
+    "run expectedly" in {
+      P08_EliminateConsecutiveDuplicatesOfListElements.compress(
+        List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+      ) mustEqual List('a, 'b, 'c, 'a, 'd, 'e)
+    }
+  }
+}
+
+object P09Spec extends Specification {
+  "P09.pack(List[T])" should {
+    "run expectedly" in {
+      P09_PackConsecutiveDuplicatesOfListElementsIntoSubLists.pack(
+        List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+      ) mustEqual List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+    }
+    "run expectedly with empty List" in {
+      P09_PackConsecutiveDuplicatesOfListElementsIntoSubLists.pack(Nil) mustEqual Nil
+    }
+  }
+}
+
+object P10Spec extends Specification {
+  "P10.encode(List[T])" should {
+    "run expectedly" in {
+      P10_RunLengthEncodingOfAList.encode(
+        List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+      ) mustEqual List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))
+    }
+  }
+  "P10.encode2(List[T])" should {
+    "run expectedly" in {
+      P10_RunLengthEncodingOfAList.encode2(
+        List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+      ) mustEqual List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))
     }
   }
 }
